70493038d9fc0889c1fd2507c5afcbc5c3e7e412
tuanvu2924 <118045716+tuanvu2924@users.noreply.github.com>
2022-11-15 17:25:01 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -61,7 +61,7 @@ EOF
 
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
-        echo "muaclone$(random)/pass$(random)/$IP4/$port/$(gen64 $IP6)"
+        echo "muaclone$(random)/$(random)/$IP4/$port/$(gen64 $IP6)"
     done
 }
 
@@ -91,8 +91,8 @@ IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 
-FIRST_PORT=1000
-LAST_PORT=2999
+FIRST_PORT=22000
+LAST_PORT=23999
 
 gen_data >$WORKDIR/data.txt
 gen_iptables >$WORKDIR/boot_iptables.sh
